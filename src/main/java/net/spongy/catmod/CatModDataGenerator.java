01196a68cbce2add2907bc599bdf5c31efe0cd0f
BuildTools <unconfigured@null.spigotmc.org>
2023-04-12 19:05:34 +0800

    Initial commit

---
+++ src/main/java/net/spongy/catmod/CatModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.spongy.catmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CatModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

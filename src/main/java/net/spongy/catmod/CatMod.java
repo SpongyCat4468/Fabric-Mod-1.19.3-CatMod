7a143b79b14cccfb905b3da865eda9cd5489f3f2
BuildTools <unconfigured@null.spigotmc.org>
2023-04-12 20:39:22 +0800

    New item: Citrine (but missing textures)

--- src/main/java/net/spongy/catmod/CatMod.java
+++ src/main/java/net/spongy/catmod/CatMod.java
@@ -2,6 +2,7 @@ package net.spongy.catmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.spongy.catmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,10 +12,7 @@ public class CatMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		ModItems.registerModItems();
 
-		LOGGER.info("Hello Fabric world!");
 	}
 }
